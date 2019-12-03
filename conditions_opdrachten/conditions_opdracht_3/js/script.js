b97c96293eb306cae529eabdab7b2bc033737dd0
Aryanto <2107921@talnet.nl>
2019-12-03 13:44:23 +0100

    Opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").nodeValue;
+
+    if(saldo==500) {
+        document.getElementById("tekst").innerHTML = "uw salaris is gestort";
+    } else if(saldo>=500) {
+        document.getElementById("tekst").innerHTML = "Uw heeft een bonus gekregen";
+    }
+    else{
+        document.getElementById("tekst").innerHTML = "Uw salaris is niet gestort";
+        
+    }
+}
