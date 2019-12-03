f51db1b7f28ac01bffd1659a6795d6ef4e60e5cb
Aryanto <2107921@talnet.nl>
2019-12-03 13:49:24 +0100

    Opdracht 4

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+function check() {
+    var mynumber = document.getElementById("saldo").Value;
+
+    if(mynumber == 10) {
+        document.getElementById("tekst").innerHTML = "De variable heeft de waarde: " + mynumber;
+    }
+}
