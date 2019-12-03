d1ba1a132d82169328ac44e1fca372baba911ba5
Aryanto <2107921@talnet.nl>
2019-12-03 13:56:06 +0100

    Opdracht 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+calculate = function()
+{
+    var resources = document.getElementById('a1').nodeValue;
+    var minutes = document.getElementById('a2').nodeValue;
+    document.getElementById('a3').value = parseInt(resources)+parseInt(minutes);
+    
+}
